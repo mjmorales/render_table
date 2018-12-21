139a90905c0247a724e2ad458d8daef2de8e7113
manuel morales <morales.jmanuel16@gmail.com>
2018-12-21 11:19:23 -0500

    Fixed indentation issues.

--- spec/render_table/row_spec.rb
+++ spec/render_table/row_spec.rb
@@ -1,21 +1,21 @@
 RSpec.describe RenderTable::Row do
-    let(:records) { [1, '1', 1.0] }
-    let(:header) { [:class, :to_s, :to_i, :to_f] }
-    let(:table) { RenderTable::Table.new(records: records, header: header) }
-    before(:each) do
-      table.html[:rows][:classes] = {
-        'is-integer' => ->(record, _row_index) { record.is_a? Integer },
-        'is-first' => ->(_record, row_index) { row_index.zero? },
-        'is-last' => ->(_record, row_index) { row_index == 2 }
-      }
+  let(:records) { [1, '1', 1.0] }
+  let(:header) { %i[class to_s to_i to_f] }
+  let(:table) { RenderTable::Table.new(records: records, header: header) }
+  before(:each) do
+    table.html[:rows][:classes] = {
+      'is-integer' => ->(record, _row_index) { record.is_a? Integer },
+      'is-first' => ->(_record, row_index) { row_index.zero? },
+      'is-last' => ->(_record, row_index) { row_index == 2 }
+    }
 
-      table.html[:rows][:ids] = {
-        'first' => ->(_record, row_index) { row_index.zero? },
-        'last' => ->(_record, row_index) { row_index == 2 }
-      }
-    end
+    table.html[:rows][:ids] = {
+      'first' => ->(_record, row_index) { row_index.zero? },
+      'last' => ->(_record, row_index) { row_index == 2 }
+    }
+  end
 
-    subject { RenderTable::Row.new(records[0], 0, table) }
+  subject { RenderTable::Row.new(records[0], 0, table) }
 
   describe '.class' do
     it 'returns "is-integer is-first" for first row' do
@@ -37,7 +37,7 @@ RSpec.describe RenderTable::Row do
     end
 
     it 'returns a collection of RenderTable::Cell objects' do
-      expect(@cells).to all( be_an(RenderTable::Cell) )
+      expect(@cells).to all(be_an(RenderTable::Cell))
     end
   end
 end
