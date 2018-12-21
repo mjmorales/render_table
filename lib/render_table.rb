847ddd9c1be4ad2ae45e3ab16fa64ea5770afec3
manuel morales <morales.jmanuel16@gmail.com>
2018-12-21 09:06:25 -0500

    Initial commit.

---
+++ lib/render_table.rb
@@ -0,0 +1,5 @@
+require 'render_table/version'
+
+module RenderTable
+  # Your code goes here...
+end
---
+++ spec/render_table_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe RenderTable do
+  it 'has a version number' do
+    expect(RenderTable::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
